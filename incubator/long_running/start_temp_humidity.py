9495fce300038e0c3fcf06643b5754d4a526a638
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-14 12:05:41 -0400

    first commit

---
+++ incubator/long_running/start_temp_humidity.py
@@ -0,0 +1,25 @@
+import board
+from adafruit_dht import DHT22
+import configparser
+import time
+
+import click
+from  paho.mqtt import publish
+import board
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+dht22 = DHT22(board.D21)
+sampling_rate = 1/float(config['temp_humidity']['samples_per_second'])
+
+
+while True:
+    time.sleep(sampling_rate)
+    try:
+        h, t = dht22.humidity, dht22.temperature
+        publish.single("incubator/humidity", h)
+        publish.single("incubator/temperature", t)
+    except (RuntimeError, OverflowError) as error:
+       print(error.args[0])
