dfec1fc55a7793713bc0ece825a81c1bf3e8dea5
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-20 10:58:02 -0400

    some robust error handling

--- incubator/long_running/start_camera.py
+++ incubator/long_running/start_camera.py
@@ -14,15 +14,16 @@ config.read('config.ini')
 sampling_rate = 1/float(config['camera']['samples_per_second'])
 hostname = config['mqtt']['hostname']
 camera = PiCamera()
+publish.single("incubator/1/log", "start_camera started", hostname=hostname)
 
 
 while True:
     sleep(sampling_rate - 0.5)
     try:
         stream = BytesIO()
-        sleep(0.5)
         camera.capture(stream, 'jpeg')
         b = bytearray(stream.getvalue())
-        publish.single("incubator/camera", b, hostname=hostname)
-    except RuntimeError as error:
-       print(error.args[0])
+        publish.single("incubator/1/camera", b, hostname=hostname)
+        publish.single("incubator/1/log", "photo taken", hostname=hostname)
+    except Exception as e:
+       pass
--- incubator/long_running/start_temp_humidity.py
+++ incubator/long_running/start_temp_humidity.py
@@ -2,10 +2,10 @@ import board
 from adafruit_dht import DHT22
 import configparser
 import time
+import RPi.GPIO as GPIO
 
 import click
 from  paho.mqtt import publish
-import board
 
 
 config = configparser.ConfigParser()
@@ -14,13 +14,17 @@ config.read('config.ini')
 dht22 = DHT22(board.D21)
 sampling_rate = 1/float(config['temp_humidity']['samples_per_second'])
 hostname = config['mqtt']['hostname']
+publish.single("incubator/1/log", "start_temp_humidity started", hostname=hostname)
 
 
 while True:
     time.sleep(sampling_rate)
     try:
         h, t = dht22.humidity, dht22.temperature
-        publish.single("incubator/humidity", h, hostname=hostname)
-        publish.single("incubator/temperature", t, hostname=hostname)
+        publish.single("incubator/1/humidity", h, hostname=hostname)
+        publish.single("incubator/1/temperature", t, hostname=hostname)
     except (RuntimeError, OverflowError) as error:
-       print(error.args[0])
+        print(error.args[0])
+        # restart
+        GPIO.cleanup()
+        dht22 = DHT22(board.D21)
