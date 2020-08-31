dbd13b68867e53bb0cb9b973df0c6850556687f2
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-08-31 13:28:46 -0400

    use bme280

--- incubator/long_running/start_temp_humidity.py
+++ incubator/long_running/start_temp_humidity.py
@@ -1,17 +1,20 @@
 import board
-from adafruit_dht import DHT22
 import configparser
 import time
-import RPi.GPIO as GPIO
+import board
+import busio
+import adafruit_bme280
 
 import click
 from  paho.mqtt import publish
 
 
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-dht22 = DHT22(board.D21)
 sampling_rate = 1/float(config['temp_humidity']['samples_per_second'])
 hostname = config['mqtt']['hostname']
 publish.single("incubator/1/log", "start_temp_humidity started", hostname=hostname)
@@ -20,11 +23,9 @@ publish.single("incubator/1/log", "start_temp_humidity started", hostname=hostna
 while True:
     time.sleep(sampling_rate)
     try:
-        h, t = dht22.humidity, dht22.temperature
+        h, t = bme280.humidity, bme280.temperature
         publish.single("incubator/1/humidity", h, hostname=hostname)
         publish.single("incubator/1/temperature", t, hostname=hostname)
     except (RuntimeError, OverflowError) as error:
         print(error.args[0])
-        # restart
-        GPIO.cleanup()
-        dht22 = DHT22(board.D21)
+
